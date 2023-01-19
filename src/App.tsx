0a0841281f504f6a79a67ac4d3dbb4fbcfbf8da4
brunof <brunof@unimedvaledocai.com.br>
2023-01-19 16:25:51 -0300

    Altaraco

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import {  } from 'react'
-import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
-    <>
-        <Button variant='primary'/>
-        <Button variant='secondary'/>
-        <Button variant='danger'/>
-        <Button variant='success'/>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
